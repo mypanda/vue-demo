a0d9f2c84fca21b3c0902788c27110e6feec4a05
mypanda <951788234@qq.com>
2017-02-28 16:50:37 +0800

    first commit

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+    	<h1>welcome come vue {{ msg }}</h1>
+    	<my-menu></my-menu>
+    </div>
+</template>
+<script>
+export default{
+    data(){
+        return {
+            msg:'welcome vue ^)^'
+        }
+    },
+    components:{
+        'my-menu':{
+        	template:`
+        		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt delectus libero laborum magni provident modi, asperiores, rem aliquam recusandae? Illum maxime dicta recusandae perspiciatis harum quaerat dolorem ullam velit laboriosam.</p>
+        	`
+        }
+    }
+}
+</script>
+<style scope>
+	.container{
+		width: 960px;
+		margin: 0 auto;
+	}
+</style>
---
+++ main.js
@@ -0,0 +1,9 @@
+import Vue from 'Vue'//必须用babel编译ES6
+import App from './App.vue'
+
+new Vue({
+    el:'#box',
+    components:{
+        app:App
+    }
+})
